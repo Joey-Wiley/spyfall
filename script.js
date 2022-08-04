720c6fdb0241e16deb869e2cdf6f4f1b2ca02b5b
Joey-Wiley <110587565+Joey-Wiley@users.noreply.github.com>
2022-08-03 23:18:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -144,11 +144,12 @@ function playersDone() {
 					textContainer.style.width = "auto";
 					textContainer.innerHTML = "Hand the phone back to its owner. The game can start now!<br><br><span id='locationsList' class='innerButton'><img id='shareIcon' class='buttonIcon' src='icons/share-icon.svg'>List of Locations</span>";
 					document.getElementById("locationsList").addEventListener("click", function() {
+						window.open(https://joey-wiley.github.io/spyfall/locations, '_blank');
 						if (navigator.share) {
 						  navigator.share({
 						    title: document.title,
-						    text: "Location List",
-						    url: "https://joey-wiley.github.io/spyfall/locations"
+						    text: "",
+						    url: https://joey-wiley.github.io/spyfall/locations
 						  })
 						  .then(() => console.log('Successful share'))
 						  .catch(error => console.log('Error sharing:', error));
